0eb99fef0c5587ef322ac43a896965979f00f385
Rafal Przywarski <rafal.przywarski@gmail.com>
2019-06-30 16:31:28 +0200

    exluded pgm files from rsyncing

--- rpi_run.sh
+++ rpi_run.sh
@@ -10,7 +10,7 @@ DEST_DIR=/home/pi/remote_builds/hcc
 
 echo "synchronising..."
 ssh ${RPI_ADDRESS} "mkdir -p ${DEST_DIR}"
-rsync -av --delete --exclude xcode --exclude Debug --exclude Release * ${RPI_ADDRESS}:${DEST_DIR}/
+rsync -av --delete --exclude xcode --exclude Debug --exclude Release --exclude "*.pgm" * ${RPI_ADDRESS}:${DEST_DIR}/
 
 echo "running..."
 ssh ${RPI_ADDRESS} "cd ${DEST_DIR}; ./run.sh"
