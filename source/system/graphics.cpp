44f3e0ee5ea74f07869bc012caa8554ac789c63f
Rafal Przywarski <rafal.przywarski@gmail.com>
2019-07-16 21:07:21 +0200

    structured text alignment and colour

--- source/system/graphics.cpp
+++ source/system/graphics.cpp
@@ -29,10 +29,10 @@ constexpr int VA_CENTER = 1;
 constexpr int VA_BASELINE_CENTER = 2;
 constexpr int VA_TOP = 3;
 
-constexpr int V_JUSTIFY = -2;
-constexpr int V_LEFT = -1;
-constexpr int V_CENTER = 0;
-constexpr int V_RIGHT = 1;
+constexpr int V_LEFT = 0;
+constexpr int V_CENTER = 1;
+constexpr int V_RIGHT = 2;
+constexpr int V_JUSTIFY = 3;
 
 constexpr std::uint32_t CC_FIRST = 0xe000;
 constexpr std::uint32_t CC_SET_ALPHA = 0xe000;
@@ -40,6 +40,7 @@ constexpr std::uint32_t CC_SET_RED = 0xe100;
 constexpr std::uint32_t CC_SET_GREEN = 0xe200;
 constexpr std::uint32_t CC_SET_BLUE = 0xe300;
 constexpr std::uint32_t CC_RESET = 0xe400;
+constexpr std::uint32_t CC_SET_ALIGN = 0xe500;
 constexpr std::uint32_t CC_LAST = 0xefff;
 
 #define HCC_GRAPHICS_TO_LINEAR \
@@ -879,7 +880,7 @@ std::int64_t text(
 {
     x *= state->display_scale; y *= state->display_scale;
     width *= state->display_scale; height *= state->display_scale;
-    const auto sc_r = c_r, sc_g = c_g, sc_b = c_b, sc_a = c_a;
+    const auto sc_r = c_r, sc_g = c_g, sc_b = c_b, sc_a = c_a, salign = align;
     auto& font = ::state->fonts.at(font_id);
     auto calc_pen_x = [&](std::int64_t line_width)
                           {
@@ -890,6 +891,18 @@ std::int64_t text(
                               default: return x * font.precision;
                               }
                           };
+    auto run_modifier = [&](std::uint32_t code)
+                            {
+                                switch (code & 0xff00)
+                                {
+                                case CC_SET_ALPHA: c_a = code & 0xff; break;
+                                case CC_SET_RED: c_r = code & 0xff; break;
+                                case CC_SET_GREEN: c_g = code & 0xff; break;
+                                case CC_SET_BLUE: c_b = code & 0xff; break;
+                                case CC_RESET: c_a = sc_a; c_r = sc_r; c_g= sc_g; c_b = sc_b; align = salign; break;
+                                case CC_SET_ALIGN: align = code & 0xff; break;
+                                }
+                            };
 
     switch (valign)
     {
@@ -905,6 +918,15 @@ std::int64_t text(
     while (*text)
     {
         auto line = fit_text_line(font, width * font.precision, text);
+        while (text < line.second)
+        {
+            auto ch = decode_utf8_char(text);
+            if (ch.first < CC_FIRST || ch.first > CC_LAST)
+                break;
+            text += ch.second;
+            run_modifier(ch.first);
+        }
+
         auto pen_x = calc_pen_x(line.first);
         auto extra_width = width * font.precision - line.first;
         auto ws_count = std::count(text, line.second, ' ');
@@ -916,14 +938,7 @@ std::int64_t text(
             if (ch.first >= CC_FIRST && ch.first <= CC_LAST)
             {
                 text += ch.second;
-                switch (ch.first & 0xff00)
-                {
-                case CC_SET_ALPHA: c_a = ch.first & 0xff; break;
-                case CC_SET_RED: c_r = ch.first & 0xff; break;
-                case CC_SET_GREEN: c_g = ch.first & 0xff; break;
-                case CC_SET_BLUE: c_b = ch.first & 0xff; break;
-                case CC_RESET: c_a = sc_a; c_r = sc_r; c_g= sc_g; c_b = sc_b; break;
-                }
+                run_modifier(ch.first);
                 continue;
             }
 
